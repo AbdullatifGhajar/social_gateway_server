f9c7603cdd8dd475d955fc9a70616fd2f5de34b7
Pascal <pascal.fuehrlich@student.hpi.de>
2019-05-28 11:53:49 +0200

    write answers to csv

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import random
 from datetime import datetime
+import csv
 
 app = Flask(__name__)
 
@@ -25,12 +26,18 @@ def send_question():
     app_id = int(request.args.get('app_id', '0'))
     return random.choices(questions)[0].replace('<app_name>', app_names[app_id])
 
-@app.route('/answer')
+answers_csv_writer = csv.DictWriter(
+    open('answers.csv', 'a', newline='', buffering=1),
+    ('user_id', 'date', 'app_id', 'question_id', 'answer'))
+answers_csv_writer.writeheader()
+
+@app.route('/answer', methods=('POST',))
 def receive_answer():
-    app_id = request.args.get('app_id', 'NULL')
-    date = datetime.utcnow().isoformat()
-    question_id = request.args.get('question_id', 'NULL')
-    answer = request.args.get('answer', 'NULL')
-    user_id = request.args.get('user_id', 'NULL')
-    print(user_id, date, app_id, question_id, answer, sep=', ')
+    answers_csv_writer.writerow({
+        'user_id': request.args.get('user_id', 'NULL'),
+        'date': datetime.utcnow().isoformat(),
+        'app_id': request.args.get('app_id', 'NULL'),
+        'question_id': request.args.get('question_id', 'NULL'),
+        'answer': request.args.get('answer', 'NULL'),
+    })
     return 'Thanks for your answer!'
