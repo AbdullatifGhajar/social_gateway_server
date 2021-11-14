4f480a6b09a85eaec1e21d18ac9ed72b3934da90
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2021-11-14 12:50:56 +0100

    Clean and organize stuff

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # social_gateway_server
+
+To change the questions you need to reboot the server, do this by copying the questions.json file either to this drive:
+scp path_on_your_system kdavis@fb07-neo4j.hpi.uni-potsdam.de:./social_gateway_server/questions.json or by directly editing the file using vim questions.json in this folder
+
+To kill the server, find the current process ID using ´ps -A´  
+This prints a large list of processes of which you need to find the one that has gunicorn (name of the server software) as name
+kill that process using:
+sudo kill xxxxxxx
+In which xxxxxxx is the process id (pid) you just found
+
+To reboot the server run this command in the current folder:
+/home/kdavis/.local/share/virtualenvs/social_gateway_server-8ajIfuiW/bin/gunicorn -w 1 -b 0.0.0.0:7474 server:app
+The response should be similar to this:
+[2021-09-05 17:16:07 +0200] [16622] [INFO] Starting gunicorn 19.9.0
+[2021-09-05 17:16:07 +0200] [16622] [INFO] Listening at: http://0.0.0.0:7474 (16622)
+[2021-09-05 17:16:07 +0200] [16622] [INFO] Using worker: sync
+[2021-09-05 17:16:07 +0200] [16625] [INFO] Booting worker with pid: 16625
+
+You can test whether the server is running by visiting https://hpi.de/baudisch/projects/neo4j/api/question?key=hef3TF%5eVg90546bvgFVL%3EZzxskfou;aswperwrsf,c/x
+It should present you with one of the questions
+
+To download the answers.csv file use
+scp answers.csv path_on_your_system
+Which should create a copy of the file on your specified path location
+
+To download the sound files, run the scp command with -r
+scp -r /audio path_on_your_system
+For all commands here you need to have a VPN connection or be in the HPI network, use EndPointSecurity client from HPI to set this up
--- server.py
+++ server.py
@@ -8,6 +8,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+SUPPORTED_LANGUAGES = ('english',)
+DEFAULT_LANGUAGE = 'english'
+DEFAULT_QUESTION_TYPE = 'normal'
+LINE_BUFFERING = 1
+
 
 def main(testing=False, injected_questions=None, injected_write_answer=None,
          injected_write_audio=None):
@@ -24,21 +29,22 @@ def main(testing=False, injected_questions=None, injected_write_answer=None,
         questions = json.load(f)
 
         for question in questions:
-            for key in ('english', 'german'):
+            for key in SUPPORTED_LANGUAGES:
                 assert key in question
 
     # not using "with", keep the file open as long as the server is running
     # newline='' is recommended for csv, buffering=1 means line buffering
-    answers_file = open('answers.csv', 'a', newline='', buffering=1)
+    answers_file = open('answers.csv', 'a', newline='',
+                        buffering=LINE_BUFFERING)
     answers_csv_writer = csv.DictWriter(
-            answers_file, (
-                'user_id',
-                'date',
-                'app_name',
-                'question',
-                'answer_text',
-                'answer_audio_uuid',
-                ))
+        answers_file, (
+            'user_id',
+            'date',
+            'app_name',
+            'question',
+            'answer_text',
+            'answer_audio_uuid',
+        ))
 
     # only write header if file is empty
     if os.stat(answers_file.name).st_size == 0:
@@ -52,23 +58,37 @@ def main(testing=False, injected_questions=None, injected_write_answer=None,
             f.write(data)
 
 
-def key_is_invalid(key):
+def is_valid_key(key):
+    # TODO: find a better place for this
     return key != 'hef3TF^Vg90546bvgFVL>Zzxskfou;aswperwrsf,c/x'
 
 
 @app.route('/browser/question')
 def send_question():
-    if key_is_invalid(request.args.get('key', '')):
+    # TODO: return a failure, not blank
+    # TODO: use decorator for that
+    if not is_valid_key(request.args.get('key', '')):
         return ''
 
     app_name = request.args.get('app_name', 'this app')
-    question_type = request.args.get('question_type', 'normal')
-    suitable_questions = \
-        [question for question in questions
-         if ('whitelist' not in question or app_name in question['whitelist'])
-         and question.get('question_type', 'normal') == question_type]
+    question_type = request.args.get('question_type', DEFAULT_QUESTION_TYPE)
 
-    language = request.args.get('language', 'english')
+    def is_suitable(question):
+        suitable = True
+        suitable &= question.get('question_type', 'normal') == question_type
+
+        if 'whitelist' in question:
+            suitable &= app_name in question['whitelist']
+
+        if 'blacklist' in question:
+            suitable &= app_name not in question['blacklist']
+
+        return suitable
+
+    suitable_questions = [
+        question for question in questions if is_suitable(question)]
+
+    language = request.args.get('language', DEFAULT_LANGUAGE)
     question_id = request.args.get('question_id',
                                    randrange(len(suitable_questions)))
 
@@ -78,7 +98,7 @@ def send_question():
 
 @app.route('/browser/answer', methods=('POST',))
 def receive_answer():
-    if key_is_invalid(request.args.get('key', '')):
+    if not is_valid_key(request.args.get('key', '')):
         return ''
 
     data = request.get_json(force=True)
@@ -96,9 +116,11 @@ def receive_answer():
 
 @app.route('/browser/audio', methods=('POST',))
 def receive_audio():
-    if key_is_invalid(request.args.get('key', '')):
+    if not is_valid_key(request.args.get('key', '')):
         return ''
 
+    # TODO: return errors
+    # TODO: rename assertions
     if 'uuid' not in request.args.keys():
         return 'UUID is required.'
 
