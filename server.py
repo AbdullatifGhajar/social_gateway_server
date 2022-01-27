0df3e0f9fde3a0fcd281daa29ed5ac5ffb61e30d
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2022-01-27 15:44:43 +0100

    Add privacy policy

--- server.py
+++ server.py
@@ -1,26 +1,31 @@
-from flask import Flask, request
 import csv
-from datetime import datetime
 import json
 import os
 import random
-import dotenv
+from datetime import datetime
 from functools import wraps
+
+import dotenv
+from flask import Flask, render_template, request
 
 dotenv.load_dotenv()  # take environment variables from .env.
 
 
 app = Flask(__name__)
 
-SUPPORTED_LANGUAGES = ('english',)
-DEFAULT_LANGUAGE = 'english'
-DEFAULT_PROMPT_TYPE = 'normal'
+SUPPORTED_LANGUAGES = ("english",)
+DEFAULT_LANGUAGE = "english"
+DEFAULT_PROMPT_TYPE = "normal"
 LINE_BUFFERING = 1
-KEY = os.environ.get('KEY')
+KEY = os.environ.get("KEY")
 
 
-def main(testing=False, injected_prompts=None, injected_write_answer=None,
-         injected_write_audio=None):
+def main(
+    testing=False,
+    injected_prompts=None,
+    injected_write_answer=None,
+    injected_write_audio=None,
+):
     global prompts, write_answer, write_audio
 
     # dependency injection for testing
@@ -30,7 +35,7 @@ def main(testing=False, injected_prompts=None, injected_write_answer=None,
         write_audio = injected_write_audio
         return
 
-    with open('prompts.json') as input_file:
+    with open("prompts.json") as input_file:
         prompts = json.load(input_file)
 
         for prompt in prompts:
@@ -39,17 +44,18 @@ def main(testing=False, injected_prompts=None, injected_write_answer=None,
 
     # not using "with", keep the file open as long as the server is running
     # newline='' is recommended for csv, buffering=1 means line buffering
-    answers_file = open('answers.csv', 'a', newline='',
-                        buffering=LINE_BUFFERING)
+    answers_file = open("answers.csv", "a", newline="", buffering=LINE_BUFFERING)
     answers_csv_writer = csv.DictWriter(
-        answers_file, (
-            'user_id',
-            'date',
-            'app_name',
-            'prompt',
-            'answer_text',
-            'answer_audio_uuid',
-        ))
+        answers_file,
+        (
+            "user_id",
+            "date",
+            "app_name",
+            "prompt",
+            "answer_text",
+            "answer_audio_uuid",
+        ),
+    )
 
     # only write header if file is empty
     if os.stat(answers_file.name).st_size == 0:
@@ -59,100 +65,95 @@ def main(testing=False, injected_prompts=None, injected_write_answer=None,
         answers_csv_writer.writerow(row)
 
     def write_audio(file_name, data):
-        with open(file_name, 'wb') as output_file:
+        with open(file_name, "wb") as output_file:
             output_file.write(data)
 
 
 def key_required(func):
     @wraps(func)
     def check_key(*args, **kwargs):
-        if(request.args.get('key', '') == KEY):
+        if request.args.get("key", "") == KEY:
             return func(*args, **kwargs)
         else:
-            return {
-                'message': 'invalid key'
-            }, 401
+            return {"message": "invalid key"}, 401
 
     return check_key
 
 
-@app.route('/browser/prompt')
+@app.route("/browser/prompt")
 @key_required
 def send_prompt():
-    app_name = request.args.get('app_name', 'this app')
-    prompt_type = request.args.get('prompt_type', DEFAULT_PROMPT_TYPE)
+    app_name = request.args.get("app_name", "this app")
+    prompt_type = request.args.get("prompt_type", DEFAULT_PROMPT_TYPE)
 
     def is_suitable(prompt):
         suitable = True
-        suitable &= prompt.get('prompt_type', 'normal') == prompt_type
+        suitable &= prompt.get("prompt_type", "normal") == prompt_type
 
-        if 'whitelist' in prompt:
-            suitable &= app_name in prompt['whitelist']
+        if "whitelist" in prompt:
+            suitable &= app_name in prompt["whitelist"]
 
-        if 'blacklist' in prompt:
-            suitable &= app_name not in prompt['blacklist']
+        if "blacklist" in prompt:
+            suitable &= app_name not in prompt["blacklist"]
 
         return suitable
 
-    suitable_prompts = [
-        prompt for prompt in prompts if is_suitable(prompt)]
+    suitable_prompts = [prompt for prompt in prompts if is_suitable(prompt)]
 
-    language = request.args.get('language', DEFAULT_LANGUAGE)
+    language = request.args.get("language", DEFAULT_LANGUAGE)
     chosen_prompt = random.choice(suitable_prompts)
 
     return {
-        'content': chosen_prompt[language].replace('<app_name>', app_name),
-        'answerable': chosen_prompt['answerable']
+        "content": chosen_prompt[language].replace("<app_name>", app_name),
+        "answerable": chosen_prompt["answerable"],
     }
 
 
-@app.route('/browser/answer', methods=('POST',))
+@app.route("/browser/answer", methods=("POST",))
 @key_required
 def receive_answer():
     data = request.get_json(force=True)
-    write_answer({
-        'date': datetime.utcnow().isoformat(),
-        'user_id': data.get('user_id', 'NULL'),
-        'app_name': data.get('app_name', 'NULL'),
-        'prompt': data.get('prompt', 'NULL'),
-        'answer_text': data.get('answer_text', 'NULL'),
-        'answer_audio_uuid': data.get('answer_audio_uuid', 'NULL'),
-    })
+    write_answer(
+        {
+            "date": datetime.utcnow().isoformat(),
+            "user_id": data.get("user_id", "NULL"),
+            "app_name": data.get("app_name", "NULL"),
+            "prompt": data.get("prompt", "NULL"),
+            "answer_text": data.get("answer_text", "NULL"),
+            "answer_audio_uuid": data.get("answer_audio_uuid", "NULL"),
+        }
+    )
     # if answer_audio_uuid is set the data should be sent to /audio
-    return {
-        'message': 'Thanks for your answer!'
-    }
+    return {"message": "Thanks for your answer!"}
 
 
-@app.route('/browser/audio', methods=('POST',))
+@app.route("/browser/audio", methods=("POST",))
 @key_required
 def receive_audio():
     # TODO: rename assertions
-    if 'uuid' not in request.args.keys():
-        return {
-            'message': 'UUID is required.'
-        }, 400
+    if "uuid" not in request.args.keys():
+        return {"message": "UUID is required."}, 400
 
     if not request.content_length:
-        return {
-            'message': 'Audio data is required.'
-        }, 400
+        return {"message": "Audio data is required."}, 400
 
-    if request.content_length > 5 * 10**6:
-        return {
-            'message': 'File is too big: %s byte.' % request.content_length
-        }, 400
+    if request.content_length > 5 * 10 ** 6:
+        return {"message": "File is too big: %s byte." % request.content_length}, 400
 
-    write_audio('audio/' + request.args["uuid"] + '.aac', request.get_data())
+    write_audio("audio/" + request.args["uuid"] + ".aac", request.get_data())
 
-    return {
-        'message': 'Thanks for your audio answer!'
-    }
+    return {"message": "Thanks for your audio answer!"}
+
+
+@app.route("/browser/privacy")
+def privacy_policy():
+    return render_template("privacy.html")
 
 
 main()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
         # ssl_context='adhoc',
-        host='0.0.0.0')
+        host="0.0.0.0"
+    )
--- test_server.py
+++ test_server.py
@@ -1,79 +1,91 @@
 import pytest
+
 import server
 
-questions = [{'english': f'question {i}',
-              'german': f'Frage {i}'} for i in range(5)]
-questions.append({'english': 'question <app_name>',
-                  'german': f'Frage <app_name>'})
+questions = [{"english": f"question {i}", "german": f"Frage {i}"} for i in range(5)]
+questions.append({"english": "question <app_name>", "german": f"Frage <app_name>"})
 
 
 @pytest.fixture
 def client():
-    server.app.config['TESTING'] = True
+    server.app.config["TESTING"] = True
     return server.app.test_client()
 
 
 def test_send_question(client):
     def request_question(**kwargs):
-        args = '&'.join(f'{key}={value}' for key, value in kwargs.items())
-        return client.get(f'/question?{args}').data.decode()
+        args = "&".join(f"{key}={value}" for key, value in kwargs.items())
+        return client.get(f"/question?{args}").data.decode()
 
     server.main(testing=True, injected_questions=questions)
 
-    assert request_question(question_id=3) == 'question 3'
-    assert request_question(question_id=0, language='english') == 'question 0'
+    assert request_question(question_id=3) == "question 3"
+    assert request_question(question_id=0, language="english") == "question 0"
 
-    assert request_question(question_id=2, language='german') == 'Frage 2'
-    assert request_question(question_id=1, language='german') == 'Frage 1'
+    assert request_question(question_id=2, language="german") == "Frage 2"
+    assert request_question(question_id=1, language="german") == "Frage 1"
 
-    assert request_question(question_id=5,
-                            app_name='Telegram') == 'question Telegram'
-    assert request_question(question_id=5,
-                            app_name='pointcloud',
-                            language='german') == 'Frage pointcloud'
+    assert request_question(question_id=5, app_name="Telegram") == "question Telegram"
+    assert (
+        request_question(question_id=5, app_name="pointcloud", language="german")
+        == "Frage pointcloud"
+    )
 
 
 def test_receive_answer(client):
-    expected_data = {'user_id': 'NULL',
-                     'app_name': 'NULL',
-                     'question': 'NULL',
-                     'answer_text': 'NULL',
-                     'answer_audio_uuid': 'NULL'}
+    expected_data = {
+        "user_id": "NULL",
+        "app_name": "NULL",
+        "question": "NULL",
+        "answer_text": "NULL",
+        "answer_audio_uuid": "NULL",
+    }
 
     def write_answer(row):
         for key, value in expected_data.items():
             assert row[key] == value
-        assert row['date']
+        assert row["date"]
 
     server.main(testing=True, injected_write_answer=write_answer)
 
-    response = client.post('/answer', data=b'{}').data.decode()
-    assert response == 'Thanks for your answer!'
+    response = client.post("/answer", data=b"{}").data.decode()
+    assert response == "Thanks for your answer!"
 
-    expected_data['user_id'] = 'b3f'
-    response = client.post('/answer', data=b'{"user_id": "b3f"}').data.decode()
-    assert response == 'Thanks for your answer!'
+    expected_data["user_id"] = "b3f"
+    response = client.post("/answer", data=b'{"user_id": "b3f"}').data.decode()
+    assert response == "Thanks for your answer!"
 
-    expected_data.update({'app_name': 'pushrequest',
-                          'question': 'You afraid of superintelligence?',
-                          'answer_text': 'Oh yes.'})
+    expected_data.update(
+        {
+            "app_name": "pushrequest",
+            "question": "You afraid of superintelligence?",
+            "answer_text": "Oh yes.",
+        }
+    )
     response = client.post(
-        '/answer', data=b'''{"user_id": "b3f",
+        "/answer",
+        data=b"""{"user_id": "b3f",
                              "app_name": "pushrequest",
                              "question": "You afraid of superintelligence?",
-                             "answer_text": "Oh yes."}''').data.decode()
-    assert response == 'Thanks for your answer!'
+                             "answer_text": "Oh yes."}""",
+    ).data.decode()
+    assert response == "Thanks for your answer!"
 
 
 def test_receive_audio(client):
-    assert client.post('/audio').data.decode() == 'UUID is required.'
-    assert client.post('/audio?uuid=b3f').data.decode() \
-        == client.post('/audio?uuid=b3f', data=b'').data.decode() \
-        == 'Audio data is required.'
+    assert client.post("/audio").data.decode() == "UUID is required."
+    assert (
+        client.post("/audio?uuid=b3f").data.decode()
+        == client.post("/audio?uuid=b3f", data=b"").data.decode()
+        == "Audio data is required."
+    )
 
     def write_audio(file_name, data):
-        assert file_name == 'audio/b3f.aac'
-        assert data == b'test'
+        assert file_name == "audio/b3f.aac"
+        assert data == b"test"
+
     server.main(testing=True, injected_write_audio=write_audio)
-    assert client.post('/audio?uuid=b3f', data=b'test').data.decode() \
-        == 'Thanks for your audio answer!'
+    assert (
+        client.post("/audio?uuid=b3f", data=b"test").data.decode()
+        == "Thanks for your audio answer!"
+    )
